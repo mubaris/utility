1106622fc83e48c1bddb3fa424ed4178abf060f8
Mubaris Hassan <mubarishassannk@gmail.com>
2017-04-17 11:00:53 +0530

    Fix exported name error

--- os.go
+++ os.go
@@ -5,16 +5,16 @@ import (
 	"runtime"
 )
 
-// osType returns the type of Operating System you're running
+// OsType returns the type of Operating System you're running
 // Possible values are,
 // android darwin dragonfly freebsd linux nacl netbsd openbsd plan9 solaris windows zos
-func osType() string {
+func OsType() string {
 	return runtime.GOOS
 }
 
-// osArch returns the architecture of Operating System you're running
+// OsArch returns the architecture of Operating System you're running
 // Possible values are,
 // 386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64
-func osArch() string {
+func OsArch() string {
 	return runtime.GOARCH
 }
