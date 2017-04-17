61061a071b783888635e5ca2cae0b296376339e6
Mubaris Hassan <mubarishassannk@gmail.com>
2017-04-17 10:08:11 +0530

    Implement OS details

---
+++ os.go
@@ -0,0 +1,19 @@
+package utility
+
+import (
+	"runtime"
+)
+
+// osType returns the type of Operating System you're running
+// Possible values are,
+// android darwin dragonfly freebsd linux nacl netbsd openbsd plan9 solaris windows zos
+func osType() string {
+	return runtime.GOOS
+}
+
+// osArch returns the architecture of Operating System you're running
+// Possible values are,
+// 386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64
+func osArch() string {
+	return runtime.GOARCH
+}
